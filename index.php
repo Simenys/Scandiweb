d9dc3020eab7f6e247bd4d2d9afd5d3326b6c75d
Simenys <s.zilevicius@gmail.com>
2023-04-01 10:29:15 +0300

    All categories added on page

--- index.php
+++ index.php
@@ -9,17 +9,14 @@
 
     <div class="container">
         <div class="row">
-            
-                
                 <?php
-                    $query = "SELECT * FROM DVD ";
+                    $query = "SELECT * FROM dvd ";
                     $select_all_categories_query = mysqli_query($conn, $query);
                     while($row = mysqli_fetch_assoc($select_all_categories_query)) {
                         $sku = $row['SKU'];
                         $name = $row['Name'];
                         $price = $row['Price'];
                         $size = $row['Size'];
-            
                 ?>
                 
                     <div class="col-md-3 box" style="background-color:#33475b">
@@ -29,6 +26,54 @@
                         <p><?php echo $name ?></p>
                         <p><?php echo $price ?></p>
                         <p><?php echo $size ?></p>
+                    </div>
+                       
+                <?php
+                    } 
+                ?>
+                
+                <?php
+                    $query = "SELECT * FROM book ";
+                    $select_all_categories_query = mysqli_query($conn, $query);
+                    while($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                        $sku = $row['SKU'];
+                        $name = $row['Name'];
+                        $price = $row['Price'];
+                        $weight = $row['Weight'];
+                ?>
+                
+                    <div class="col-md-3 box" style="background-color:#33475b">
+                        <input class="form-check-input delete-checkbox" type="checkbox" value="" id="flexCheckDefault">
+                        <!-- <label class="form-check-label" for="flexCheckDefault"></label> -->
+                        <p><?php echo $sku ?></p>
+                        <p><?php echo $name ?></p>
+                        <p><?php echo $price ?></p>
+                        <p><?php echo $weight ?></p>
+                    </div>
+                       
+                <?php
+                    } 
+                ?>
+
+                <?php
+                    $query = "SELECT * FROM furniture ";
+                    $select_all_categories_query = mysqli_query($conn, $query);
+                    while($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                        $sku = $row['SKU'];
+                        $name = $row['Name'];
+                        $price = $row['Price'];
+                        $dimHeight = $row['DimHeight'];
+                        $dimWidth = $row['DimWidth'];
+                        $dimLength = $row['DimLength'];
+                ?>
+                
+                    <div class="col-md-3 box" style="background-color:#33475b">
+                        <input class="form-check-input delete-checkbox" type="checkbox" value="" id="flexCheckDefault">
+                        <!-- <label class="form-check-label" for="flexCheckDefault"></label> -->
+                        <p><?php echo $sku ?></p>
+                        <p><?php echo $name ?></p>
+                        <p><?php echo $price ?></p>
+                        <p><?php echo strval($dimHeight) . 'x' . strval($dimWidth) . 'x' . strval($dimLength) ?></p>
                     </div>
                        
                 <?php
